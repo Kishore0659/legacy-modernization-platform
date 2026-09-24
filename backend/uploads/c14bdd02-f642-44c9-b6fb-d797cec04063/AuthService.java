1f1d67dfde7af1412a1365f8d1de3812bbebd3a8
Kishore R <kishore.r18112005@gmail.com>
2026-09-24 10:55:10 +0530

    Ignore generated uploads and local cache

--- backend/uploads/c14bdd02-f642-44c9-b6fb-d797cec04063/AuthService.java
+++
@@ -1,6 +0,0 @@
-package com.modernize.auth;
-
-public interface AuthService {
-    boolean authenticate(String token);
-    void revokeSession(String userId);
-}
--- backend/uploads/e1be74e4-6bed-4e82-b260-715cc4c9ab75/BaseService.java
+++
@@ -1,9 +0,0 @@
-package com.modernize.core;
-
-public abstract class BaseService {
-    protected String serviceId;
-
-    public void logAudit(String event) {
-        System.out.println("Audit Event Recorded: " + event);
-    }
-}
